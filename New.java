7921b7f09e4951352a702de61cd8a8cbcd0b9efd
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-06 10:12:12 +0530

    datatypes

---
+++ New.java
@@ -0,0 +1,10 @@
+class New {
+    public static void main(String[] args) {
+    char var1 = 'h' ;
+    String var2 = "hello" ;
+    boolean var3 = true;
+    int var5 = 90 ;
+    double var6 = 100.2544 ; 
+    System.out.println(var6);
+    } 
+}
