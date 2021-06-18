3f6bef311e4e21dcaf201bdb778c2178c620fde1
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-18 08:39:55 +0530

    new lines on maths

--- New.java
+++ New.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class New {
     public static void main(String[] args) {
     char var1 = 'h' ;
@@ -5,6 +7,8 @@ class New {
     boolean var3 = true;
     int var5 = 90 ;
     double var6 = 100.2544 ; 
-    System.out.println(var6);
+    double var7 = 10;
+    double var8 = 15;
+    System.out.println(var7 / var8); 
     } 
 }
