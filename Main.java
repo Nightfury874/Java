7abcd7e254783f4c63dda9f6aef38f3efa244c69
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-18 09:18:27 +0530

    scanner

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
 class Main{
     public static void main (String [] args){
-        System.out.println("Hello");
+        Scanner scan =  new Scanner(System.in);
+        String txt = scan.next();
+        System.out.println(txt);
     }
 }
