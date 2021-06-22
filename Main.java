1e97b00548aeb619854fa58a9cd234aa2d74cc9a
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-22 21:20:50 +0530

    anuthing

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ class Main{
     public static void main (String [] args){
         String a = "heloo";
         double b = 1.5;
+        double c = 2.5;
+        double d = 3.5;
+        double e = 2.5;
+
+        boolean try1 = e==c;
+        System.out.println (try1);
     }
 }
