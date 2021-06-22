073452cc2a97ce3fce7ef3af07c87ba68b4dc298
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-22 22:05:38 +0530

    if_else

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse {
+  public static void main(String[] args) {
+    String a = "hello";
+    double b = 5.25;
+    double c = 7.25;
+
+    if (c>b){
+        System.out.println("Ok");
+    }
+    else{
+        System.out.println("not ok");
+        }
+  }  
+}
