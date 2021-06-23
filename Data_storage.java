7aa2ef391cb5d9639e6924204245a24f4efb30eb
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-23 10:10:02 +0530

    array

---
+++ Data_storage.java
@@ -0,0 +1,13 @@
+public class Data_storage {
+    public static void main(String[] args) {
+        double[] array  = new double[5];
+        array[0] = 1;
+        array[1] = 2;
+
+
+        System.out.println(array[1]);
+
+        
+    }
+    
+}
