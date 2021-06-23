b7511c7e87946613df94ccec000db3007e2ecf12
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-23 21:51:02 +0530

    array

---
+++ forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+    public static void main(String[] args) {
+
+        double[] array1 = new double [5];
+        array1[0] = 1.05;
+        array1[1] = 2.05;
+        array1[2] = 3.05;
+
+        for ( int i =0; i < array1.length; i++)
+        System.out.println(array1[i]);
+    }    
+}
+
