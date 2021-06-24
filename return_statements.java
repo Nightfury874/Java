4f42ed4c8dbc81508cc356d915d15d5d3e0aeee1
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-24 23:08:46 +0530

    nn

--- return_statements.java
+++ return_statements.java
@@ -1,14 +1,16 @@
 public class return_statements {
     public static void main (String[] args){
         name();
-        int sum =add(4,5);
-        System.out.println(sum);
-    }
-    public static void name() {
-        System.out.println("Name");
+        // int sum =add(4,5);
+        // System.out.println(sum);
+        String shout = caps("Hello my name is ");
+        System.out.println(shout);
+   }
+     public static void name() {
+         System.out.println("Name");}
         
-    }
-    public static int add(int a, int b){
-        return a+b;
+    
+    public static String caps(String s){
+        return s.toUpperCase();
     }
 }
