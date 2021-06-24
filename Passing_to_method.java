f00b10a68e9657c46474fdd55dfe8c5521d893e3
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-24 20:00:53 +0530

    passing to method

--- Passing_to_method.java
+++ Passing_to_method.java
@@ -1,12 +1,16 @@
-class Test{
+public class passing_to_method{
     public static void main(String[] args) {
-        int arr[] = {3,1,2,5,4};
+        int arr[] = {1,2,3,4,5,6};
+        System.out.println("method to print sum of arrays");
         sum(arr);
     }
-}
-
-public class Passing_to_method {
-
-    
-    
+    public static void sum(int[] arr) {
+        int sum = 0;
+        for (int i=0; i < arr.length; i++){
+            sum += arr[i];
+             
+        }
+        System.out.println(sum);
+        
+    }
 }
