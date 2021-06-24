7db22cd131fc39796d577ae80ab8d8d91f724015
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2021-06-24 16:16:22 +0530

    array new

--- array.java
+++ array.java
@@ -1,13 +1,23 @@
-import java.util.Scanner;
-
+// import java.util.Scanner;
+class Student
+{
+    public int roll_no;
+    public String name;
+    Student (int roll_no, String name)
+    {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}
 public class array {
     public static void main(String[] args) {
-        int[] array1 = new int []{1,2,3,4,5,56,7,8,9,10};
-        Scanner scan = new Scanner ( System.in);
-        int x =  scan.nextInt();  
-        System.out.println (x); 
-        for (int i =0; i< x; i++){
-            System.out.println( "The element at index " + i + " is " +array1[i]);
+        Student [] array1 = new Student[5];
+        array1[0] = new Student(1, "Nihar");
+        array1[1] =  new Student (2, "Jyoti");
+        array1[2] =  new Student (3, "Basisth");
+
+        for (int i =0; i< array1.length; i++){
+            System.out.println("The name of student at psition 1 is " + array1[i].name + "and roll number is " + array1[i].roll_no);
         }
 
     }
